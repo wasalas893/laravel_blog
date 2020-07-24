2a0ed87729cb3a8cf20e43af6a5dbfc979e48bb8
wasalas <wasalas893@gmail.com>
2020-07-24 06:16:41 +0530

    connect the database and post route to create

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,6 +20,8 @@ Route::get('/','PagesController@index');
 Route::get('/about','PagesController@about');
 Route::get('/services','PagesController@services');
 
+Route::resource('posts','PostsController');
+
 
 
 
